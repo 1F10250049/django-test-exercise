a72d2b98292ec110b3ff766326390d4094fdef6e
1F10250049 <s1f102500494@iniad.org>
2026-07-01 13:30:30 +0900

    詳細ページのテストを追加

--- todo/tests.py
+++ todo/tests.py
@@ -42,8 +42,24 @@ class TaskModelTestCase(TestCase):
         task.save()
         self.assertFalse(task.is_overdue(current))
 
+def test_detail_get_fail(self):
+        client = Client()
+        response = client.get('/1/')
+        
+        self.assertEqual(response.status_code, 404)
 
 class TodoViewTestCase(TestCase):
+
+    def test_detail_get_success(self):
+        task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task.save()
+        client = Client()
+        response = client.get('/{}/'.format(task.pk))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'todo/detail.html')
+        self.assertEqual(response.context['task'], task)
+
     def test_index_get(self):
         client = Client()
         response = client.get('/')
